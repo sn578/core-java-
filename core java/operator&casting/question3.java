127b943cb547c0570af34dc9c1a3e843a4c48eb1
sn578 <sneharajak421@gmail.com>
2025-09-10 20:11:06 +0530

    Add files via upload

---
+++ core java/operator&casting/question3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class question3{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the first number:");
+        int a = sc.nextInt();
+        System.out.println("before swapping: " +a);
+        int b = sc.nextInt();
+        System.out.println("befor swapping: " +b);
+        a = a + b;
+        a = a - b;
+        b = a - b; 
+           System.out.println("\nAfter swapping:");
+           System.out.println("a = " + a + ", b = " + b);
+    }
+}
