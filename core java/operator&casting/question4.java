d9d472631a02690225fdcf271bb7a0947515ce96
sn578 <sneharajak421@gmail.com>
2025-09-10 20:20:02 +0530

    Add files via upload

---
+++ core java/operator&casting/question4.java
@@ -0,0 +1,14 @@
+//write a program to convert int to double using implicit casting.
+public class question4{
+    public static void main(String[]args){
+        //int a = 10;
+        //double number = a;
+        //System.out.println(number);
+        /* question 2 . now  double into int  */
+        
+        double b = 1.333;
+        int number = (int)b;
+        System.out.println("before casting :" +b);
+        System.out.println("After casting :"+number);
+    }
+}
